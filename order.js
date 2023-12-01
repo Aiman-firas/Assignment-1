88f40202db234d654984e139e4458c3ba814028b
Aiman <s10258376@connect.np.edu.sg>
2023-12-01 12:35:02 +0800

    Order Page near finalization

--- order.js
+++ order.js
@@ -99,3 +99,130 @@ document.addEventListener("DOMContentLoaded", function () {
     cupcakeName.textContent = detail.name;
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const selectButtons = document.querySelectorAll(".select-options button");
+
+  selectButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const category = button
+        .closest(".select-block")
+        .querySelectorAll("button");
+      category.forEach((btn) => btn.classList.remove("selected"));
+
+      button.classList.add("selected");
+
+      updateCupcakePrice();
+    });
+  });
+
+  const candlesInput = document.getElementById("candlesQuantity");
+  candlesInput.addEventListener("change", function () {
+    const quantity = parseInt(candlesInput.value);
+    if (isNaN(quantity) || quantity < 0) {
+      candlesInput.value = 0;
+    }
+  });
+});
+
+function updateCupcakePrice() {
+  const sizeButtons = document.querySelectorAll(
+    ".select-block:nth-child(1) button.selected"
+  );
+  const priceElement = document.querySelector(".cupcake-price");
+
+  if (sizeButtons.length > 0) {
+    const selectedSize = sizeButtons[0].innerText.trim();
+
+    switch (selectedSize) {
+      case "One":
+        priceElement.innerText = "$3.75";
+        break;
+      case "Half-Dozen":
+        priceElement.innerText = "$21.50";
+        break;
+      case "Dozen":
+        priceElement.innerText = "$41.50";
+        break;
+      default:
+        priceElement.innerText = "$3.75";
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const addToCartButton = document.getElementById("addToCartButton");
+  addToCartButton.addEventListener("click", function () {
+    addToCart();
+  });
+
+  sizeButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      handleButtonClick(button, ".select-size");
+      updateCupcakePrice();
+    });
+  });
+
+  frostingButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      handleButtonClick(button, ".select-frosting");
+    });
+  });
+
+  topperButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      handleButtonClick(button, ".select-topper");
+    });
+  });
+
+  const candlesInput = document.getElementById("candlesQuantity");
+  candlesInput.addEventListener("change", function () {
+    const quantity = parseInt(candlesInput.value);
+    if (isNaN(quantity) || quantity < 0) {
+      candlesInput.value = 0;
+    }
+  });
+
+  addToCartButton.addEventListener("click", function () {
+    addToCart();
+  });
+});
+
+function handleButtonClick(clickedButton, categorySelector) {
+  const categoryButtons = document.querySelectorAll(
+    `${categorySelector} button`
+  );
+  categoryButtons.forEach((button) => button.classList.remove("selected"));
+  clickedButton.classList.add("selected");
+}
+function addToCart() {
+  const cupcakeName = document.querySelector(".cupcake-name").textContent;
+  const cupcakePrice = document.querySelector(".cupcake-price").textContent;
+  const cupcakeSize = document.querySelector(
+    ".select-size button.selected"
+  ).textContent;
+  const cupcakeFrostingType = document.querySelector(
+    ".select-frosting button.selected"
+  ).textContent;
+  const cupcakeTopper = document.querySelector(
+    ".select-topper button.selected"
+  ).textContent;
+  const numberOfCandles = document.getElementById("candlesQuantity").value;
+
+  const cartItem = {
+    name: cupcakeName,
+    price: cupcakePrice,
+    size: cupcakeSize,
+    frostingType: cupcakeFrostingType,
+    topper: cupcakeTopper,
+    candles: numberOfCandles,
+  };
+
+  const existingCart = JSON.parse(localStorage.getItem("cart")) || [];
+  existingCart.push(cartItem);
+
+  localStorage.setItem("cart", JSON.stringify(existingCart));
+
+  window.location.href = "cart.html";
+  alert("Item added to cart!");
+}
