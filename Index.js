2172a59cf13266f6144847a450ba58eed4ff30b2
Aiman <s10258376@connect.np.edu.sg>
2023-12-01 20:21:15 +0800

    SplashScreen Update

--- Index.js
+++ Index.js
@@ -1,3 +1,29 @@
+/*Splash Screen*/
+let intro = document.querySelector(".intro");
+let logo = document.querySelector(".intro-logo-header");
+let logoSpan = document.querySelectorAll(".intro-logo");
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (idx + 1) * 400);
+    });
+
+    setTimeout(() => {
+      logoSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (idx + 1) * 50);
+      });
+    }, 2000);
+    setTimeout(() => {
+      intro.style.top = "-100vh";
+    }, 2300);
+  });
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   var intervalTime = 3000;
   var slideIndex = 0;
