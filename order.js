779cc15d055d4fe7a8a34ba58b2b21f1b08cb9a0
Aiman <s10258376@connect.np.edu.sg>
2023-12-01 09:45:45 +0800

    creation of order.js

---
+++ order.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const cupcakeType = urlParams.get("cupcake");
+
+  const cupcakeDetails = {
+    caramel: {
+      image: "img/Caramel-Cupcake.jpeg",
+      rating: "5.0",
+      name: "Classic Caramel Perfection",
+    },
+    vanilla: {
+      image: "img/Vanilla-Cupcake.webp",
+      rating: "4.9",
+      name: "Classic Vanilla Dream",
+    },
+    "red-velvet": {
+      image: "img/Velvet-Cupcake.jpg",
+      rating: "4.8",
+      name: "Red Velvet Elegance",
+    },
+    "cookies-cream": {
+      image: "img/Oreo-Cupcake.webp",
+      rating: "4.8",
+      name: "Cookies & Cream",
+    },
+    "s-mores": {
+      image: "img/S'Mores-Cupcake.jpg",
+      rating: "4.7",
+      name: "S-Mores",
+    },
+    "chocolate-euphoria": {
+      image: "img/Chocolate-Cupcake.jpg",
+      rating: "4.7",
+      name: "Chocolate Euphoria",
+    },
+    "strawberry-bliss": {
+      image: "img/Strawberry-Cupcake.jpg",
+      rating: "4.6",
+      name: "Strawberry Bliss",
+    },
+    "espresso-love": {
+      image: "img/Espresso-Cupcake.jpeg",
+      rating: "4.8",
+      name: "Espresso Love",
+    },
+    "salted-caramel": {
+      image: "img/SaltedCaramel-Cupcake.jpg",
+      rating: "4.7",
+      name: "Salted Caramel Bliss",
+    },
+    "blueberry-burst": {
+      image: "img/Blueberry-Cupcake.jpg",
+      rating: "4.5",
+      name: "Blueberry Burst",
+    },
+    "coconut-paradise": {
+      image: "img/Coconut-Cupcake.webp",
+      rating: "4.5",
+      name: "Coconut Paradise",
+    },
+    "lemon-zest": {
+      image: "img/Lemon-Cupcake.avif",
+      rating: "4.6",
+      name: "Luscious Lemon Zest",
+    },
+  };
+
+  setDetails(cupcakeDetails[cupcakeType]);
+
+  function setDetails(detail) {
+    if (!detail) {
+      console.error("Invalid cupcake type or not provided.");
+      return;
+    }
+
+    const orderImage = document.querySelector(".inside-image img");
+    const cupcakeRating = document.querySelector(".rating-text");
+    const cupcakeName = document.querySelector(".cupcake-name");
+
+    orderImage.src = detail.image;
+
+    cupcakeRating.textContent = detail.rating;
+
+    cupcakeName.textContent = detail.name;
+  }
+});
